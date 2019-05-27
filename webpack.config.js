dc5bce4f6a59d5e38a523d2b89e29eba25078c4f
carxinho <hocarxin@163.com>
2019-05-27 16:17:39 +0800

    webpack初试

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,31 @@
+const path = require('path');
 
-module.exports = {  
-    entry: './src/app.js',  //入口文件
-    output: {  //输出文件路径设置
-        path: __dirname,  
-        filename: './bin/app.bundle.js',  
-    },  
-    module: {  
-        loaders: [{  
-            test: /\.js$/,  
-            exclude: /node_modules/,  
-            loader: 'babel-loader'  
-        }]  
-    }  
-} 
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist/js/')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            }
+        ]
+    }
+};
